fafd3bbc46573bc9c19a3143685247b5abf3d8c7
Rishi Chourasia <rishichourasia2100@gmail.com>
2022-03-27 19:15:48 +0530

    [feat] Added components routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./components/Homepage/HomePage";
+import { ProductList } from "./components/Productlist/ProductList";
+import { Cart } from "./components/Cart/Cart";
+import { Wishlist } from "./components/Wishlist/Wishlist";
+import { Login } from "./components/Login/Login";
+import { Signup } from "./components/Signup/Signup";
 
 function App() {
-	return <div className="App">Hello World</div>;
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<h1>Hello Ecommerce</h1>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/productlist" element={<ProductList />} />
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/wishlist" element={<Wishlist />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Wishlist/Wishlist.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Wishlist = () => {
+	return <div>Wishlist</div>;
+};
