e1f4585d7442508b060100256814987e2e99f1db
Rishi Chourasia <rishichourasia2100@gmail.com>
2024-05-13 13:00:02 +0530

    [fix] Fix homepage design and responsiveness

--- src/components/Homepage/HomePage.jsx
+++ src/components/Homepage/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./homepage.css";
 import { Link } from "react-router-dom";
+import HeroComponent from "../../icons";
 
 const HomePage = () => {
-	return (
-		<>
-			<div className="img-container">
+  return (
+    <>
+      {/* <div className="img-container">
 				<img
 					className="homepage-img"
 					src="/assests/pictures/homepage-img-cropped.jpg"
@@ -19,44 +20,63 @@ const HomePage = () => {
 						<button className="btn btn-primary">View Books</button>
 					</Link>
 				</div>
-			</div>
-			<div className="desc-container">
-				<div className="feature">
-					<div className="feature-head">
-						<img src="/assests/pictures/icons8-care-100.png" alt="vector-img" />
-					</div>
-					<div className="feature-body">
-						<p className="feature-title">Handpicked Books</p>
-						<p className="feature-desc">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-							Asperiores, alias, officia dicta nihil fugiat eum tempora.
-						</p>
-						<Link to="/productlist">
-							<button className="btn btn-primary">View Books</button>
-						</Link>
-					</div>
-				</div>
-				<div className="feature">
-					<div className="feature-head">
-						<img
-							src="/assests/pictures/icons8-books-100.png"
-							alt="vector-img"
-						/>
-					</div>
-					<div className="feature-body">
-						<p className="feature-title">Original Print</p>
-						<p className="feature-desc">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-							Asperiores, alias, officia dicta nihil fugiat eum tempora.
-						</p>
-						<Link to="/productlist">
-							<button className="btn btn-primary">View Books</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+			</div> */}
+      <div className="hero-container">
+        <div className="hero-title">
+          <p>
+            Transform your personal wellbeing with{" "}
+            <span className="highlight-text">self-help</span> books
+          </p>
+          <Link to="/productlist">
+            <button
+              className="btn btn-primary home-btn"
+              style={{ width: "120px", height: "35px", marginTop: "20px" }}
+            >
+              Buy Now
+            </button>
+          </Link>
+        </div>
+        <div className="hero-image">
+          <HeroComponent />
+        </div>
+      </div>
+      <div className="desc-container">
+        <div className="feature">
+          <div className="feature-head">
+            <img src="/assests/pictures/icons8-care-100.png" alt="vector-img" />
+          </div>
+          <div className="feature-body">
+            <p className="feature-title">Handpicked Books</p>
+            <p className="feature-desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, alias, officia dicta nihil fugiat eum tempora.
+            </p>
+            <Link to="/productlist">
+              <button className="btn btn-primary-outlined">View Books</button>
+            </Link>
+          </div>
+        </div>
+        <div className="feature">
+          <div className="feature-head">
+            <img
+              src="/assests/pictures/icons8-books-100.png"
+              alt="vector-img"
+            />
+          </div>
+          <div className="feature-body">
+            <p className="feature-title">Original Print</p>
+            <p className="feature-desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, alias, officia dicta nihil fugiat eum tempora.
+            </p>
+            <Link to="/productlist">
+              <button className="btn btn-primary-outlined">View Books</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export { HomePage };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,46 +3,41 @@ import "../../styles/navbar.css";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<>
-			{/* navbar  */}
-			<header className="nav fixed">
-				<div className="menu-logo">
-					<div className="menu-btn">
-						<Link to="/">
-							<img
-								className="logo-svg"
-								src="/assests/logo/logo simple dark.svg"
-								alt="logo-svg"
-							/>
-						</Link>
-					</div>
-				</div>
-				<div className="nav-links">
-					<Link to="/wishlist">
-						<div className="hover">
-							<i className="far fa-heart fa-lg" />
-						</div>
-					</Link>
-					<Link to="/cart">
-						<div className="hover">
-							<i className="far fa-shopping-cart fa-lg" />
-						</div>
-					</Link>
-					<div className="hover">
-						<i className="fas is-dark fa-sign-out-alt fa-lg" />
-					</div>
-				</div>
-				<div className="nav-btn">
-					<Link to="/login">
-						<button className="btn btn-primary">Login</button>
-					</Link>
-					<Link to="/signup">
-						<button className="btn btn-primary-outlined">Signup</button>
-					</Link>
-				</div>
-			</header>
-			{/* navbar */}
-		</>
-	);
+  return (
+    <>
+      {/* navbar  */}
+      <header className="nav fixed">
+        <div className="menu-logo">
+          <div className="menu-btn">
+            <Link to="/">
+              <img
+                className="logo-svg"
+                src="/assests/logo/logo simple dark.svg"
+                alt="logo-svg"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="nav-cta">
+          <Link to="/wishlist">
+            <div className="hover">
+              <i className="far fa-heart fa-lg" />
+            </div>
+          </Link>
+          <Link to="/cart">
+            <div className="hover">
+              <i className="far fa-shopping-cart fa-lg" />
+            </div>
+          </Link>
+          <div className="hover">
+            <i className="fas is-dark fa-sign-out-alt fa-lg" />
+          </div>
+          <Link to="/login">
+            <button className="btn  btn-primary-outlined">Login</button>
+          </Link>
+        </div>
+      </header>
+      {/* navbar */}
+    </>
+  );
 };
